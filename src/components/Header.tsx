ef82ccc42e77f737a4e070fa1515bd7822b3f98e
Aditya-Srivastava <pks85715@gmail.com>
2025-06-27 19:13:34 +0530

    file initialised

---
+++ src/components/Header.tsx
@@ -0,0 +1,26 @@
+import { Sparkles } from "lucide-react";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
+          <div className="flex items-center space-x-3">
+            <div className="p-2 bg-gradient-to-r from-white via-zinc-300 to-zinc-400 rounded-lg">
+              <Sparkles className="h-8 w-8 text-zinc-600" />
+            </div>
+            <div>
+              <h1 className="text-3xl font-bold ">Data Alchemist</h1>
+              <p className="">
+                Transform your spreadsheet chaos into organized, validated data
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
